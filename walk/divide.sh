e80003fe5161f3fd7f1d43d748848cfe6dcd0144
mizuno <mizuno@nowhere.co.jp>
2016-11-30 20:42:24 +0900

    motions

--- walk/divide.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-for i in `seq 1 10`
-do
-    read filename
-    mkdir "$filename"
-    mv walk-??-"$filename"* "$filename"
-done
--- woman_behaviour/divide.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-for i in `seq 1 10`
-do
-    read filename
-    mkdir "$filename"
-    mv woman_behaviour-??-"$filename"* "$filename"
-done
